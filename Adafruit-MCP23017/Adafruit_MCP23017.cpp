36ad1eea5db809372fba8eca223a76cc2cb1b80f
Tenkai Kariya <tenkai@zetaohm.com>
2016-02-10 17:42:10 -0800

    modified MCP23017 to work with 120mhz

--- Adafruit-MCP23017/Adafruit_MCP23017.cpp
+++ Adafruit-MCP23017/Adafruit_MCP23017.cpp
@@ -107,7 +107,7 @@ void Adafruit_MCP23017::begin(uint8_t addr) {
 	}
 	i2caddr = addr;
 
-	Wire.begin(I2C_MASTER, 0, 0, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_1800);
+	Wire.begin(I2C_MASTER, 0, 0, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_800);
 
 	// set defaults!
 	// all inputs on port A and B
